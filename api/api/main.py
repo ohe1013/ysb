11b5f3f43fc40e874f0c2684f70525ddbd92d597
orslow <jueonpk@gmail.com>
2022-12-26 23:41:58 +0900

    Add CORS config

--- api/api/main.py
+++ api/api/main.py
@@ -5,12 +5,27 @@ from hashlib import sha256
 import jwt
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .db import *
 from .models import *
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/test")
